56717d26ef6dacc0d72829cd9b48786c94e2390b
zerg959 <zerg959@yandex.ru>
2024-07-15 21:55:28 +0300

    CHANGE: step 3. help otions modify

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,9 +4,10 @@ import { program } from 'commander';
 import gendiff from '../src/index.js';
 
 program
+  .helpOption('-h, --help', 'output usage information')
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format (default: "stylish")')
+  .option('-f, --format [type]', 'output format') //  (default: "stylish")
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     // console.log(gendiff(filepath1, filepath2));
