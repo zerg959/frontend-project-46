3671327bd7fd78b7b0cb67808293b00ab6419a91
zerg959 <zerg959@yandex.ru>
2024-10-09 22:45:14 +0300

    tests chek

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import resultStylish from '../__fixtures__/result';
-import resultPlain from '../__fixtures__/reultPlain.js';
-import resultJSON from '../__fixtures__/reultJSON.js';
+import resultPlain from '../__fixtures__/resultPlain.js';
+import resultJSON from '../__fixtures__/resultJSON.js';
 import gendiff from '../src/index.js';
 
 const testList = [
@@ -16,7 +16,7 @@ describe('gendiff', () => {
     const filepath1 = resolvePath(`file1.${format}`);
     const filepath2 = resolvePath(`file2.${format}`);
 
-    expect(gendiff(filepath1, filepath2).toEqual(resultStylish));
+    expect(gendiff(filepath1, filepath2)).toEqual(resultStylish);
     expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(resultStylish);
     expect(gendiff(filepath1, filepath2, 'plain')).toEqual(resultPlain);
     expect(gendiff(filepath1, filepath2, 'json')).toEqual(resultJSON);
